6f5e7606c74b27dc456264ab7835c0bd093d5da5
lindseyleplae <lindseyleplae@gmail.com>
2026-07-21 08:35:29 -0700

    deploy: UGC safety

---
+++ app/assets/orgDashboardUrl-1x8eUWGF.js
@@ -0,0 +1,1 @@
+import{ah as a}from"./app-CgM1UmqP.js";function o(n){return a(`manage/${encodeURIComponent(n)}`)}export{o as m};
--- app/assets/orgDashboardUrl-BeIiPKk2.js
+++
@@ -1,1 +0,0 @@
-import{af as a}from"./app-Dkc-9Huv.js";function o(n){return a(`manage/${encodeURIComponent(n)}`)}export{o as m};
