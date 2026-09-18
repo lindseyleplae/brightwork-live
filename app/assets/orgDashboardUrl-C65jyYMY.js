21312d896715df4e13064655332f9a4893b67821
lindseyleplae <lindseyleplae@gmail.com>
2026-09-18 12:09:27 -0700

    deploy: East Bay (Oakland & Berkeley) is live

--- app/assets/orgDashboardUrl-C65jyYMY.js
+++
@@ -1,1 +0,0 @@
-import{ak as a}from"./app-4R53FWwI.js";function o(n){return a(`manage/${encodeURIComponent(n)}`)}export{o as m};
---
+++ app/assets/orgDashboardUrl-DU1W99ea.js
@@ -0,0 +1,1 @@
+import{al as a}from"./app-DFDFrix2.js";function o(n){return a(`manage/${encodeURIComponent(n)}`)}export{o as m};
