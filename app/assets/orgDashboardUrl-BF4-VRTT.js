e49020db7e7a59a93e12b924b32723139c94e196
lindseyleplae <lindseyleplae@gmail.com>
2026-09-19 09:59:40 -0700

    deploy: smallgood is always lowercase

---
+++ app/assets/orgDashboardUrl-BF4-VRTT.js
@@ -0,0 +1,1 @@
+import{an as a}from"./app-BEsRT0DN.js";function o(n){return a(`manage/${encodeURIComponent(n)}`)}export{o as m};
--- app/assets/orgDashboardUrl-COnQQ-1o.js
+++
@@ -1,1 +0,0 @@
-import{an as a}from"./app-BfNuZdD6.js";function o(n){return a(`manage/${encodeURIComponent(n)}`)}export{o as m};
