238806bfd012fd767785e1da241279e479d1ba0c
lindseyleplae <lindseyleplae@gmail.com>
2026-09-19 17:37:45 -0700

    Deploy: anonymous launch funnel counts + privacy sentence; App Store button built in but off

--- app/assets/orgDashboardUrl-BF4-VRTT.js
+++
@@ -1,1 +0,0 @@
-import{an as a}from"./app-BEsRT0DN.js";function o(n){return a(`manage/${encodeURIComponent(n)}`)}export{o as m};
---
+++ app/assets/orgDashboardUrl-ZRufgdKN.js
@@ -0,0 +1,1 @@
+import{an as a}from"./app-CfjeXSp1.js";function o(n){return a(`manage/${encodeURIComponent(n)}`)}export{o as m};
